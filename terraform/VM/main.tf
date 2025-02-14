c6b3ecbcb3cb64516a47394c2c53cb7d86812c58
anuja2015 <armdevu15@gmail.com>
2025-02-14 19:31:43 +0530

    Adding output.tf

--- terraform/VM/main.tf
+++ terraform/VM/main.tf
@@ -1,9 +1,16 @@
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.18.0"
+        source = "hashicorp/azurerm"
+        version = "=3.0.0"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    
   }
 }
 
