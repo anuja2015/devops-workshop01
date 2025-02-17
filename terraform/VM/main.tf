b7bd1a60207ba02bc3fb25b11d8c27cbd7ddd9a6
anuja2015 <armdevu15@gmail.com>
2025-02-17 17:57:21 +0530

    changed vm size

--- terraform/VM/main.tf
+++ terraform/VM/main.tf
@@ -98,7 +98,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     network_interface_ids = [azurerm_network_interface.nic01.id]
-    size = "Standard_D2s_v3"
+    size = "Standard_D4s_v3"
     os_disk {
       name = "terra-osdisk"
       caching = "ReadWrite"
